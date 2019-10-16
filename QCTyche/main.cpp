980801e9a146c28f8787dea449f4845e73881b39
Tobias Hahnen <hahnen.tobi@gmail.com>
2019-10-16 12:55:58 +0200

    Creation of repo
    Added royale libarary, added cvui.

---
+++ QCTyche/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+#define CVUI_IMPLEMENTATION
+#include <cvui.h>
+
+#include <royale.hpp>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
